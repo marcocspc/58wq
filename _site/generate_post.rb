d506fe7f88d8a1b37809de9e1ec9e315f016a70b
marcocspc <marcocspc@hotmail.com>
2020-05-21 04:28:44 -0300

    testing custom theme in github

---
+++ _site/generate_post.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/ruby
+
+puts "Insira o título do seu post:"
+post_title = gets.gsub("\n", "")
+
+time = Time.new
+time_str = time.strftime("%Y-%m-%d")
+tz = time.zone + "00"
+date_str = time.strftime("%Y-%m-%d %k:%M:%S " + tz)
+
+
+
+post_content = """---
+layout: post
+title:  \"#{post_title}\"
+date:  #{date_str} 
+categories: cat1 cat2 cat3  
+---
+
+# #{post_title} 
+
+Só um exemplo de conteúdo.
+"""
+
+filename = time_str + "-" + post_title + ".md" 
+filename = filename.gsub(/[\x00\/\\:\*\?\"<>\|]/, "-")
+filename = filename.gsub(" ", "-")
+
+
+File.write('./_posts/' + filename, post_content)
