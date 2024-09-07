30f1639eb1618c0f1202f740aeca0c18bd7f3219
marcocspc <marco.araujo.dev@outlook.com>
2024-09-07 19:59:12 -0300

    fix: strings inside generate_draft.py

--- generate_draft.py
+++ generate_draft.py
@@ -7,21 +7,21 @@ post_title = input("Insira o título do seu rascunho:")
 time = datetime.now()
 time_str = time.strftime("%Y-%m-%d")
 tz = datetime.now().astimezone().tzname()
-date_str = time.strftime("%Y-%m-%d %k:%M:%S " + tz)
+date_str = time.strftime("%Y-%m-%d %k:%M:%S " + tz + "00")
 
 
 
 post_content = """---
 layout: post
-title:  \"#{post_title}\"
-date:  #{date_str} 
+title:  \"{post_title}\"
+date:  {date_str} 
 categories: cat1 cat2 cat3  
 ---
 
-# #{post_title} 
+# {post_title} 
 
 Só um exemplo de conteúdo.
-"""
+""".format(post_title=post_title, date_str=date_str)
 
 filename = time_str + "-" + post_title + ".md" 
 filename = "".join([x if x.isalnum() or x in '.' else "-" for x in filename])
